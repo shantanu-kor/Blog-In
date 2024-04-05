24a7b4278fc17573378fe8cb54eca55b748f08c5
Shantanu Nitin Kor <kor.shantanu1@gmail.com>
2024-04-05 19:24:02 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignUp from './pages/SignUp';
+import LogIn from './pages/LogIn';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+
 const App = () => {
   return (
-    <div className="text-3xl text-red-500">App</div>
+    <React.Fragment>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/Sign-up" element={<SignUp />} />
+        <Route path="/Log-in" element={<LogIn />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="projects" element={<Projects />} />
+      </Routes>
+    </React.Fragment>
   )
 }
 
